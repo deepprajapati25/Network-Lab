7452b035161017c0ed9b47943f2c4ce261ed7aa2
Pranav Patil <106877570+iPranav-Patil@users.noreply.github.com>
2023-04-27 00:00:51 +0530

    Update varaitions.java

--- varaitions.java
+++ varaitions.java
@@ -12,9 +12,25 @@ String reversed = "";
             System.out.println("The input string is not a palindrome.");
         }
 
+-----------------------------------------------------------------------------
+
 #Factorial in java
  int factorial = 1;
 
     for (int i = 1; i <= n; i++) {
       factorial *= i;
+    }
+
+
+-----------------------------------------------------------------------------
+#vowels extract
+private static String extractVowels(String sentence) {
+        StringBuilder vowels = new StringBuilder();
+        for (int i = 0; i < sentence.length(); i++) {
+            char c = sentence.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+                vowels.append(c);
+            }
+        }
+        return vowels.toString();
     }
